dc90756a9bee98fd51c4bf7cf86474446a55062e
Bernie FitzGerald <bernardf@live.com.au>
2023-06-24 17:14:03 +1000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useDebugValue } from 'react';
 
-function App() {
+const useWindowWidth = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener on unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  //useDebugValue(windowWidth);
+  //useDebugValue(getScreenSizeLabel(windowWidth));
+  useDebugValue(getScreenSizeLabel(windowWidth), (value) => `Screen Size: ${value}`);
+  return windowWidth;
+};
+
+const getScreenSizeLabel = (width) => {
+  if (width < 576) {
+    return 'Extra Small (xs)';
+  } else if (width < 768) {
+    return 'Small (sm)';
+  } else if (width < 992) {
+    return 'Medium (md)';
+  } else if (width < 1200) {
+    return 'Large (lg)';
+  } else {
+    return 'Extra Large (xl)';
+  }
+};
+
+// Usage example
+const App = () => {
+  const windowWidth = useWindowWidth();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Window Width: {windowWidth}px</p>
     </div>
   );
-}
+};
 
 export default App;
